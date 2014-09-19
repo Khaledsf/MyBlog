d0024ab917b190089fc3190c42f99280ae0dd720
Khaledsf <kbouchama@gmail.com>
2014-09-18 23:48:57 -0700

    server handles more requests

--- server.rb
+++ server.rb
@@ -1,7 +1,13 @@
 require 'rack'
 require 'ap'
+require 'sqlite3'
+require 'erubis'
+require 'uri'
 
 class App
+    def initialize
+        @db = SQLite3::Database.new "kbblog.db"
+    end
     def call(env)
         ap env
         case env["REQUEST_METHOD"]
@@ -14,37 +20,61 @@ class App
 
     private
     def get(env)
-        status_code = 404
-        headers = {
-            "Content-Type" => "text/html"
-        }
-        body = ["<h1>404 Not Found</h1>"]
+        req = Rack::Request.new(env)
+        response = Rack::Response.new
+        response.status = 404
+        response["Content-Type"] = "text/html"
+        response.body = ["<h1>404 Not Found</h1>"]
 
         if env["PATH_INFO"] == "/"
-            status_code = 200
-            body = [File.open(File.expand_path((File.dirname(__FILE__))) + "/public/index.html", "rb").read]
+            response = renderIndex(req)
         else
-            path = File.expand_path("#{File.dirname(__FILE__)}/public/#{env['PATH_INFO']}")
+            path = File.expand_path("public/#{env['PATH_INFO']}")
             if File.exists?(path)
-                status_code = 200
-                body = [File.open(path, "rb").read]
+                response.status = 200
+                response.body = [File.open(path, "rb").read]
             end
         end
 
-        [status_code,headers,body]
+        response.finish
     end
 
     def post(env)
-        [
-            200,
-            {
-                'Content-Type' => 'text/html'
-            },
-            [
-                "Gettin' racked."
-            ]
-        ]
+        req = Rack::Request.new(env)
+        response = Rack::Response.new
+        response.status = 200
+        # Login always works!
+        if env["PATH_INFO"] == "/login"
+            if req.params["fname"] == "khaled" && req.params["lname"] == "bouchama"
+                response["Content-Type"] = "text/html"
+                response.body = [File.open("public/admin.html", "rb").read]
+            else
+                response.status = 401
+                response["Content-Type"] =  "text/html"
+                response.body = ["<h1>401 Unauthorized</h1>"]
+            end
+        elsif env["PATH_INFO"] == "/posts/new"
+            # Execute a few inserts
+            ap req.params
+            post = req.params['post'].sub("'", "\'")
+            post = req.params['post'].sub('"', '\"')
+            @db.execute "insert into posts(title,post) values ('#{req.params['title']}', '#{post}')"
+            response = renderIndex(req)
+        end
+
+        response.finish
+    end
+
+    def renderIndex(req)
+        @posts = @db.execute "select * from posts"
+        ap @posts
+        renderer = Erubis::Eruby.new(File.open("public/index.html", "rb").read)
+        response = Rack::Response.new
+        response.status = 200
+        response["Content-Type"] = "text/html"
+        response.body = [renderer.result({posts: @posts})]
+        # response.body = [File.open("public/index.html", "rb").read]
+        response
     end
 end
-
-Rack::Handler::WEBrick.run(App.new, :Port => 1234)
+Rack::Handler::WEBrick.run(App.new, :Port => 8080)
